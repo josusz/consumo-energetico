07b23748993bd5ab22fbd854ea94e7ea70bf47e3
josusz <josuhcarvalho@gmail.com>
2023-04-18 11:29:15 -0300

    criação inicial das variáveis

--- src/app/calcular-consumo/calcular-consumo.component.ts
+++ src/app/calcular-consumo/calcular-consumo.component.ts
@@ -6,5 +6,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./calcular-consumo.component.css']
 })
 export class CalcularConsumoComponent {
+  numMoradores: number=0;
+  numComodos: number=0;
+  numMaquina: number=0;
+  numSecadora: number=0;
+  numTV: number=0;
+  numPC: number=0;
+  valorTarifa: number=0;
+  consumoEletrico: number=0;
+  valorConta: number=0;
 
+  calcular(){
+    
+  }
 }
