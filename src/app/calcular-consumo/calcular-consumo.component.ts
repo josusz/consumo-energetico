2e7066c956a5e4f7433b9a46414d14c2b9ae1938
josusz <josuhcarvalho@gmail.com>
2023-04-19 02:35:40 -0300

    implementação inicial da lógica de cálculo

--- src/app/calcular-consumo/calcular-consumo.component.ts
+++ src/app/calcular-consumo/calcular-consumo.component.ts
@@ -8,8 +8,8 @@ import { Component } from '@angular/core';
 export class CalcularConsumoComponent {
   numMoradores: number=0;
   numComodos: number=0;
-  numMaquina: number=0;
-  numSecadora: number=0;
+  temMaquina: string="Não";
+  temSecadora: string="Não";
   numTV: number=0;
   numPC: number=0;
   valorTarifa: number=0;
@@ -17,6 +17,18 @@ export class CalcularConsumoComponent {
   valorConta: number=0;
 
   calcular(){
+        const consumoChuveiro = (this.numMoradores * 4) / 2;
+        const consumoMaquina = this.temMaquina === "Sim" ? 0.70 : 0;
+        const consumoSecadora = this.temSecadora === "Sim" ? 4 : 0;
+        const consumoTV = this.numTV * 13.5;
+        const consumoPC = this.numPC * 0.60; 
     
-  }
+        const consumoTotal = consumoChuveiro + consumoMaquina + consumoSecadora + consumoTV + consumoPC;
+    
+        const consumoMensal = consumoTotal * 30;
+    
+        this.valorConta = consumoMensal * this.valorTarifa;
+    
+        this.consumoEletrico = consumoMensal;
+    }
 }
